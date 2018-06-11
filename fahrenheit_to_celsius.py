329ca7dfac00aa92a8baebe6a3620b337a284abe
Mario Martinez <40052727+marioBytes@users.noreply.github.com>
2018-06-11 06:51:30 -0600

    Add files via upload

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,9 @@
+import math
+
+#get celsius
+celsius = float(input('What is the temperature in Celsius? '))
+
+#calculate celsius to fahrenheit
+fahrenheit = float(1.8 * celsius + 32)
+
+print(fahrenheit)
